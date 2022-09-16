03dffddc5c16fafb7ada77777515785ea9c43ee1
Mario S.O <mario@Marios-MacBook-Pro.local>
2022-09-16 11:27:38 +0200

    first commit

---
+++ src/PassDaBomb.sol
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract PassDaBomb {
+    uint256 BOMB_COUNTDOWN = block.timestamp + 24 hours;
+
+    address public currentHolder;
+    address[] public previousHolders;
+
+    address previousExploder;
+    address[] bombExploderAddresses;
+
+    event BombExploded(
+        address indexed exploder,
+        address indexed previousHolder
+    );
+    event BombPassed(address indexed newHolder, address indexed previousHolder);
+    event BombDefused(address indexed defuser);
+
+    constructor() {
+        currentHolder = msg.sender;
+    }
+
+    function transferDaBomb(address newHolder) public payable {
+        require(
+            msg.value >= 0.01 ether,
+            "You must pay 0.01 ether to get rid of da bomb"
+        );
+        require(msg.sender == currentHolder, "You are not the current holder");
+        require(
+            block.timestamp < BOMB_COUNTDOWN,
+            "The bomb has already exploded"
+        );
+
+        previousHolders.push(currentHolder);
+        currentHolder = newHolder;
+        emit BombPassed(newHolder, msg.sender);
+    }
+
+    function defuseDaBomb() public payable {
+        require(
+            msg.value >= 0.1 ether,
+            "You need to pay 0.1 ether to defuse the bomb"
+        );
+        require(msg.sender == currentHolder, "You are not the current holder");
+        require(
+            block.timestamp > BOMB_COUNTDOWN,
+            "The bomb has not exploded yet"
+        );
+
+        emit BombDefused(msg.sender);
+    }
+
+    function bombExploded() public {
+        bombExploderAddresses.push(currentHolder);
+        previousExploder = currentHolder;
+        emit BombExploded(
+            currentHolder,
+            previousHolders[previousHolders.length - 1]
+        );
+    }
+
+    // Getters
+    function getPreviousHolders() public view returns (address[] memory) {
+        return previousHolders;
+    }
+
+    function getCurrentHolder() public view returns (address) {
+        return currentHolder;
+    }
+
+    function getPreviousHolder(uint256 index) public view returns (address) {
+        return previousHolders[index];
+    }
+
+    function getPreviousHoldersCount() public view returns (uint256) {
+        return previousHolders.length;
+    }
+}
