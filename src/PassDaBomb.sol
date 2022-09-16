8458f20e9d9af60651f9c4ca32699beb4bacb6b6
Mario S.O <mario@Marios-MacBook-Pro.local>
2022-09-16 13:22:11 +0200

    Added TODO comments for missing tasks

--- src/PassDaBomb.sol
+++ src/PassDaBomb.sol
@@ -1,28 +1,67 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-// import openzeppelin contracts
+// TODO: Draw the state diagram for this contract
+
+
+// Globals: BombId
+/* Bomb Specific: 
+    bombCountdown,
+    currentHolder,
+    previousHolders,
+    previousExploders,
+    bombDefusers
+*/
+
+// States: Transfer, Defuse, Exploded
+
+/*  
+    Transfer: 
+        Update the bomb countdown,
+        current holder,
+        previous holder,
+        previous holders list
+    Defuse: 
+        Update defusers list,
+        find a way to create a new bomb,
+        push last bomb to DaBombs list
+    Exploded: 
+        Update previous exploder,
+        previous exploders list,
+        find a way to create a new bomb,
+        push last bomb to DaBombs list
+*/
+
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PassDaBomb is Ownable, ReentrancyGuard {
+    uint256 currentBombId = 0;
+
     struct DaBomb {
         uint256 bombCountdown;
         address currentHolder;
         address[] previousHolders;
         address previousExploder;
         address[] bombExploderAddresses;
+        address[] bombDefuserAddresses;
     }
 
     DaBomb[] public daBombs;
 
-    DaBomb daBomb = DaBomb({
-        bombCountdown: block.timestamp,
-        currentHolder: msg.sender,
-        previousHolders: new address[](0),
-        previousExploder: address(0),
-        bombExploderAddresses: new address[](0)
-    });
+    DaBomb daBomb =
+        DaBomb({
+            bombCountdown: block.timestamp,
+            currentHolder: msg.sender,
+            previousHolders: new address[](0),
+            previousExploder: address(0),
+            bombExploderAddresses: new address[](0),
+            bombDefuserAddresses: new address[](0)
+        });
+
+    event BombTransferred(address indexed from, address indexed to);
+    event BombDefused(address indexed from, address indexed to);
+    event BombExploded(address indexed from, address indexed to);
 
     constructor() {
         daBombs.push(daBomb);
@@ -35,7 +74,10 @@ contract PassDaBomb is Ownable, ReentrancyGuard {
                 "You must pay 0.01 ether to get rid of da bomb"
             );
         }
-        require(msg.sender == daBomb.currentHolder, "You are not the current holder");
+        require(
+            msg.sender == daBomb.currentHolder,
+            "You are not the current holder"
+        );
         require(
             block.timestamp < daBomb.bombCountdown,
             "The bomb has already exploded"
@@ -45,16 +87,20 @@ contract PassDaBomb is Ownable, ReentrancyGuard {
         daBomb.bombCountdown = block.timestamp + 1 days;
     }
 
-    function defuseDaBomb() public payable nonReentrant {
+    function defuseDaBomb(uint256 _currentBombId) public payable nonReentrant {
         require(
             msg.value >= 0.1 ether,
             "You need to pay 0.1 ether to defuse the bomb"
         );
-        require(msg.sender == daBomb.currentHolder, "You are not the current holder");
+        require(
+            msg.sender == daBomb.currentHolder,
+            "You are not the current holder"
+        );
         require(
             block.timestamp > daBomb.bombCountdown,
             "The bomb has not exploded yet"
         );
+        // TODO: add logic for a defused bomb
     }
 
     function bombExploded() public {
