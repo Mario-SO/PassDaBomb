34b1a5fec3603b991f7981b1d7b77597f18f6130
Mario S.O <gh_mario.proclaim@slmail.me>
2022-09-16 13:24:27 +0200

    Fixing stuff

--- test/PassDaBomb.t.sol
+++ test/PassDaBomb.t.sol
@@ -14,13 +14,25 @@ contract PassDaBombTest is Test {
     function test_transferDaBomb() public {
         passDaBomb.transferDaBomb(address(this));
         assert.equal(
-            passDaBomb.daBombs[0].currentHolder,
+            passDaBomb.daBombs(0).currentHolder,
             address(this),
             "current holder should be this contract"
         );
     }
 
     function test_defuseDaBomb() public {
+        passDaBomb.defuseDaBomb{value: 0.1 ether}();
+        assert.equal(
+            passDaBomb.daBombs(0).currentHolder,
+            address(0),
+            "current holder should be 0x0"
+        );
+    }
+
+    // test bombexploded function by setting block timestamp to 1 day after bomb countdown
+    function test_bombExploded() public {
+        passDaBomb.transferDaBomb(address(this));
+        block.timestamp = passDaBomb.daBombs(0).bombCountdown + 1 days;
         passDaBomb.defuseDaBomb{value: 0.1 ether}();
         assert.equal(
             passDaBomb.daBombs(0).currentHolder,
